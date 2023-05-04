784e9e9c110e5e969639761cb13a7f5fd0ff959a
Robby <robby@santoslab.org>
2023-05-04 08:20:15 -0500

    Special cased Graal on macOS/aarch64 version.

--- graal.cmd
+++ graal.cmd
@@ -30,140 +30,149 @@ def usage(): Unit = {
 
 
 val homeBin: Os.Path = Os.slashDir.up.canon
-val graalVersion = "22.3.2"
-val url = s"https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$graalVersion"
 
 val cacheDir: Os.Path = Os.env("SIREUM_CACHE") match {
   case Some(dir) => Os.path(dir)
   case _ => Os.home / "Downloads" / "sireum"
 }
 
-def mac(): Unit = {
+@strictpure def url(graalVersion: String) = s"https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-$graalVersion"
+
+def mac(isArm: B, javaVersion: String, graalVersion: String): Unit = {
   val platformDir = homeBin / "mac"
   val graalDir = platformDir / "graal"
   val ver = graalDir / "VER"
+  val version = s"$javaVersion-$graalVersion"
 
-  if (ver.exists && ver.read == graalVersion) {
+  if (ver.exists && ver.read == version) {
     return
   }
 
-  val javaV = "17"
-  val arch: String = if (ops.StringOps(proc"uname -m".runCheck().out).trim == "arm64") "aarch64" else "amd64"
-  val bundle = s"graalvm-ce-java$javaV-darwin-$arch-$graalVersion.tar.gz"
+  val arch: String = if (isArm) "aarch64" else "amd64"
+  val bundle = s"graalvm-ce-java$javaVersion-darwin-$arch-$graalVersion.tar.gz"
   val cache = cacheDir / bundle
 
   if (!cache.exists) {
     cache.up.mkdirAll()
     println(s"Downloading Graal $graalVersion ...")
-    cache.downloadFrom(s"$url/$bundle")
+    cache.downloadFrom(s"${url(graalVersion)}/$bundle")
   }
   if (graalDir.exists) {
     graalDir.removeAll()
   }
   println(s"Extracting $cache ...")
   Os.proc(ISZ("tar", "xfz", cache.string)).at(platformDir).console.runCheck()
-  (platformDir / s"graalvm-ce-java$javaV-$graalVersion" / "Contents" / "Home").moveTo(graalDir)
-  (platformDir / s"graalvm-ce-java$javaV-$graalVersion").removeAll()
+  (platformDir / s"graalvm-ce-java$javaVersion-$graalVersion" / "Contents" / "Home").moveTo(graalDir)
+  (platformDir / s"graalvm-ce-java$javaVersion-$graalVersion").removeAll()
 
-  val nativeBundle = s"native-image-installable-svm-java$javaV-darwin-$arch-$graalVersion.jar"
+  val nativeBundle = s"native-image-installable-svm-java$javaVersion-darwin-$arch-$graalVersion.jar"
   val nativeCache = cacheDir / nativeBundle
   if (!nativeCache.exists) {
     println(s"Downloading Graal's native-image ...")
-    nativeCache.downloadFrom(s"$url/$nativeBundle")
+    nativeCache.downloadFrom(s"${url(graalVersion)}/$nativeBundle")
   }
 
   Os.proc(ISZ((graalDir / "bin" / "gu").string, "install", "--file", nativeCache.string)).console.runCheck()
 
-  ver.writeOver(graalVersion)
+  ver.writeOver(version)
 
   println("... done!")
 }
 
-def linux(isArm: B): Unit = {
+def linux(isArm: B, javaVersion: String, graalVersion: String): Unit = {
   val platformDir: Os.Path = if (isArm) homeBin / "linux" / "arm" else homeBin / "linux"
   val graalDir = platformDir / "graal"
   val ver = graalDir / "VER"
+  val version = s"$javaVersion-$graalVersion"
 
-  if (ver.exists && ver.read == graalVersion) {
+  if (ver.exists && ver.read == version) {
     return
   }
 
-  val javaV = "17"
   val arch: String = if (isArm) "aarch64" else "amd64"
-  val bundle = s"graalvm-ce-java$javaV-linux-$arch-$graalVersion.tar.gz"
+  val bundle = s"graalvm-ce-java$javaVersion-linux-$arch-$graalVersion.tar.gz"
   val cache = cacheDir / bundle
 
   if (!cache.exists) {
     cache.up.mkdirAll()
     println(s"Downloading Graal $graalVersion ...")
-    cache.downloadFrom(s"$url/$bundle")
+    cache.downloadFrom(s"${url(graalVersion)}/$bundle")
   }
   if (graalDir.exists) {
     graalDir.removeAll()
   }
   println(s"Extracting $cache ...")
   Os.proc(ISZ("tar", "xfz", cache.string)).at(platformDir).console.runCheck()
-  (platformDir / s"graalvm-ce-java$javaV-$graalVersion").moveTo(graalDir)
+  (platformDir / s"graalvm-ce-java$javaVersion-$graalVersion").moveTo(graalDir)
 
-  val nativeBundle = s"native-image-installable-svm-java$javaV-linux-$arch-$graalVersion.jar"
+  val nativeBundle = s"native-image-installable-svm-java$javaVersion-linux-$arch-$graalVersion.jar"
   val nativeCache = cacheDir / nativeBundle
   if (!nativeCache.exists) {
     println(s"Downloading Graal's native-image ...")
-    nativeCache.downloadFrom(s"$url/$nativeBundle")
+    nativeCache.downloadFrom(s"${url(graalVersion)}/$nativeBundle")
   }
 
   Os.proc(ISZ((graalDir / "bin" / "gu").string, "install", "--file", nativeCache.string)).console.runCheck()
 
-  ver.writeOver(graalVersion)
+  ver.writeOver(version)
 
   println("... done!")
 }
 
-def win(): Unit = {
+def win(javaVersion: String, graalVersion: String): Unit = {
   val platformDir = homeBin / "win"
   val graalDir = platformDir / "graal"
   val ver = graalDir / "VER"
+  val version = s"$javaVersion-$graalVersion"
 
-  if (ver.exists && ver.read == graalVersion) {
+  if (ver.exists && ver.read == version) {
     return
   }
 
-  val javaV = "17"
-  val bundle = s"graalvm-ce-java$javaV-windows-amd64-$graalVersion.zip"
+  val bundle = s"graalvm-ce-java$javaVersion-windows-amd64-$graalVersion.zip"
   val cache = cacheDir / bundle
 
   if (!cache.exists) {
     cache.up.mkdirAll()
     println(s"Downloading Graal $graalVersion ...")
-    cache.downloadFrom(s"$url/$bundle")
+    cache.downloadFrom(s"${url(graalVersion)}/$bundle")
   }
   if (graalDir.exists) {
     graalDir.removeAll()
   }
   println(s"Extracting $cache ...")
   cache.unzipTo(platformDir)
-  (platformDir / s"graalvm-ce-java$javaV-$graalVersion").moveTo(graalDir)
+  (platformDir / s"graalvm-ce-java$javaVersion-$graalVersion").moveTo(graalDir)
 
-  val nativeBundle = s"native-image-installable-svm-java$javaV-windows-amd64-$graalVersion.jar"
+  val nativeBundle = s"native-image-installable-svm-java$javaVersion-windows-amd64-$graalVersion.jar"
   val nativeCache = cacheDir / nativeBundle
   if (!nativeCache.exists) {
     println(s"Downloading Graal's native-image ...")
-    nativeCache.downloadFrom(s"$url/$nativeBundle")
+    nativeCache.downloadFrom(s"${url(graalVersion)}/$nativeBundle")
   }
 
   Os.proc(ISZ((graalDir / "bin" / "gu.cmd").string, "install", "--file", nativeCache.string)).console.runCheck()
 
-  ver.writeOver(graalVersion)
+  ver.writeOver(version)
 
   println("... done!")
 }
 
 def platform(p: String): Unit = {
+  val graalVersion = "22.3.2"
+  val javaVersion = "17"
   p match {
-    case string"mac" => mac()
-    case string"linux" => linux(F)
-    case string"linux/arm" => linux(T)
-    case string"win" => win()
+    case string"mac" =>
+      val isArm: B = ops.StringOps(proc"uname -m".runCheck().out).trim == "arm64"
+      if (isArm) {
+        mac(T, "19", "22.3.1")
+      } else {
+        mac(F, javaVersion, graalVersion)
+      }
+      mac(F, javaVersion, graalVersion)
+    case string"linux" => linux(F, javaVersion, graalVersion)
+    case string"linux/arm" => linux(T, javaVersion, graalVersion)
+    case string"win" => win(javaVersion, graalVersion)
     case string"-h" => usage()
     case _ =>
       eprintln("Unsupported platform")
