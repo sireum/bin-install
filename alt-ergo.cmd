469bfcc17faa21be6c66d8eb2623ba54787048f9
Robby <robby@santoslab.org>
2024-05-16 14:14:34 -0500

    Upgraded alt-ergo.

--- alt-ergo.cmd
+++ alt-ergo.cmd
@@ -24,7 +24,7 @@ exit /B %errorlevel%
 import org.sireum._
 
 val homeBin = Os.slashDir.up.canon
-val altErgoVersion = "2.5.3"
+val altErgoVersion = "2.5.4"
 
 val cores: String = Os.cliArgs match {
   case ISZ(n) => Z(n).getOrElse(Os.numOfProcessors).string
