2485f63e65837db29e2091bb82c18b4978ec93c6
Robby <robby@santoslab.org>
2024-08-28 06:58:38 -0500

    Upgraded Sireum VSCode extension.

--- vscodium.cmd
+++ vscodium.cmd
@@ -34,7 +34,7 @@ val cacheDir: Os.Path = Os.env("SIREUM_CACHE") match {
 val isInUserHome = ops.StringOps(s"${homeBin.up.canon}${Os.fileSep}").startsWith(Os.home.string)
 
 val version = "1.92.2.24228"
-val sireumExtVersion = "4.20240826.046a3a4"
+val sireumExtVersion = "4.20240828.3b8fbbe"
 val sysIdeVersion = "0.6.2"
 val urlPrefix = s"https://github.com/VSCodium/vscodium/releases/download/$version"
 val sireumExtUrl = s"https://github.com/sireum/vscode-extension/releases/download/$sireumExtVersion/sireum-vscode-extension.vsix"
