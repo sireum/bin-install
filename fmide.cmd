0b44649df3c59a64482b80e49aa9d9f80f3f5c58
Jason Belt <belt@ksu.edu>
2024-10-28 11:11:05 -0500

    update fmide cli

--- fmide.cmd
+++ fmide.cmd
@@ -56,7 +56,7 @@ import Cli._
           |
           |Available Options:
           |    --awas               AWAS version (expects a string; default is
-          |                           "1.2024.10011227.45f8d78c")
+          |                           "1.2024.10281024.2762e991")
           |    --agree              AGREE version (expects a string; default is
           |                           "agree_2.9.1")
           |    --briefcase          BriefCASE version (expects a string; default is
@@ -64,9 +64,9 @@ import Cli._
           |    --eclipse            Eclipse release version (expects a string; default is
           |                           "2021-03")
           |    --gumbo              Sireum GUMBO version (expects a string; default is
-          |                           "1.2024.8211808.e0a2706")
+          |                           "1.2024.10181001.07c00045")
           |    --hamr               Sireum HAMR version (expects a string; default is
-          |                           "1.2024.10011227.45f8d78c")
+          |                           "1.2024.10281024.2762e991")
           |    --osate              OSATE version (expects a string; default is
           |                           "2.10.2-vfinal")
           |    --resolute           Resolute version (expects a string; default is
@@ -80,12 +80,12 @@ import Cli._
           |-v, --verbose            Verbose output
           |    --verbose+           Increased verbose output""".render
 
-    var awas: Option[String] = Some("1.2024.10011227.45f8d78c")
+    var awas: Option[String] = Some("1.2024.10281024.2762e991")
     var agree: Option[String] = Some("agree_2.9.1")
     var briefcase: Option[String] = Some("briefcase_0.8.0")
     var eclipse: Option[String] = Some("2021-03")
-    var gumbo: Option[String] = Some("1.2024.8211808.e0a2706")
-    var hamr: Option[String] = Some("1.2024.10011227.45f8d78c")
+    var gumbo: Option[String] = Some("1.2024.10181001.07c00045")
+    var hamr: Option[String] = Some("1.2024.10281024.2762e991")
     var osate: Option[String] = Some("2.10.2-vfinal")
     var resolute: Option[String] = Some("resolute_3.0.0")
     var existingInstall: Option[String] = None[String]()
