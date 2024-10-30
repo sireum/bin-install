96d835551402a041f67560ac940758d7edb958c9
Jason Belt <belt@ksu.edu>
2024-10-30 14:17:05 -0500

    update fmide versions

--- fmide.cmd
+++ fmide.cmd
@@ -56,7 +56,7 @@ import Cli._
           |
           |Available Options:
           |    --awas               AWAS version (expects a string; default is
-          |                           "1.2024.10281024.2762e991")
+          |                           "1.2024.10301208.4e3e80ef")
           |    --agree              AGREE version (expects a string; default is
           |                           "agree_2.9.1")
           |    --briefcase          BriefCASE version (expects a string; default is
@@ -66,7 +66,7 @@ import Cli._
           |    --gumbo              Sireum GUMBO version (expects a string; default is
           |                           "1.2024.10181001.07c00045")
           |    --hamr               Sireum HAMR version (expects a string; default is
-          |                           "1.2024.10281024.2762e991")
+          |                           "1.2024.10301208.4e3e80ef")
           |    --osate              OSATE version (expects a string; default is
           |                           "2.10.2-vfinal")
           |    --resolute           Resolute version (expects a string; default is
@@ -80,12 +80,12 @@ import Cli._
           |-v, --verbose            Verbose output
           |    --verbose+           Increased verbose output""".render
 
-    var awas: Option[String] = Some("1.2024.10281024.2762e991")
+    var awas: Option[String] = Some("1.2024.10301208.4e3e80ef")
     var agree: Option[String] = Some("agree_2.9.1")
     var briefcase: Option[String] = Some("briefcase_0.8.0")
     var eclipse: Option[String] = Some("2021-03")
     var gumbo: Option[String] = Some("1.2024.10181001.07c00045")
-    var hamr: Option[String] = Some("1.2024.10281024.2762e991")
+    var hamr: Option[String] = Some("1.2024.10301208.4e3e80ef")
     var osate: Option[String] = Some("2.10.2-vfinal")
     var resolute: Option[String] = Some("resolute_3.0.0")
     var existingInstall: Option[String] = None[String]()
