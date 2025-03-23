4567bfebd31d94c5b11127e68ee72e42acd9e974
Robby <robby@santoslab.org>
2025-03-23 07:03:56 -0500

    Upgraded Brave.

--- brave.cmd
+++ brave.cmd
@@ -19,7 +19,7 @@ val cacheDir: Os.Path = Os.env("SIREUM_CACHE") match {
   case _ => Os.home / "Downloads" / "sireum"
 }
 
-val version = "1.76.80"
+val version = "1.76.81"
 val urlPrefix = s"https://github.com/brave/brave-browser/releases/download/v$version/"
 
 def download(drop: Os.Path): Unit = {
