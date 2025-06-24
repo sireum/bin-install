7d2dc0a1e2a4ad3c5d8024a204b6ad3ef481120d
Robby <robby@santoslab.org>
2025-06-24 06:54:57 -0500

    Upgraded CLion.

--- clion.cmd
+++ clion.cmd
@@ -16,7 +16,7 @@ val url = s"https://download.jetbrains.com/cpp"
 
 val homeBin = Os.slashDir.up.canon
 val home = homeBin.up.canon
-val clionVersion = "2025.1.2"
+val clionVersion = "2025.1.3"
 val plugins = HashSSet.empty[String] ++ ISZ[String]("github", "gitlab", "lsp4ij", "rust", "toml")
 val init = Init(home, Os.kind, Sireum.versions)
 val clionInstallVersion: String = st"$clionVersion-${(for (pid <- plugins.elements) yield init.distroPlugins.get(pid).get.version, "-")}".render
