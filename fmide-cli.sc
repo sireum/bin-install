359cf20a503a45d99cc853d1533949b2d9f179a7
Jason Belt <belt@ksu.edu>
2026-07-13 12:22:33 -0500

    update fmide versions

--- fmide-cli.sc
+++ fmide-cli.sc
@@ -16,11 +16,11 @@ val fmideTool: Tool = Tool(
   usageDescOpt = None(),
   opts = ISZ(
     Opt(name = "awas", longKey = "awas", shortKey = None(),
-      tpe = Type.Str(sep = None(), default = Some("1.2026.05201117.a8293090")), description = "AWAS version"),
+      tpe = Type.Str(sep = None(), default = Some("1.2026.07131214.177108b3")), description = "AWAS version"),
     Opt(name = "gumbo", longKey = "gumbo", shortKey = None(),
       tpe = Type.Str(sep = None(), default = Some("1.2026.06240925.18efbba7")), description = "Sireum GUMBO version"),
     Opt(name = "hamr", longKey = "hamr", shortKey = None(),
-      tpe = Type.Str(sep = None(), default = Some("1.2026.05201117.a8293090")), description = "Sireum HAMR version"),
+      tpe = Type.Str(sep = None(), default = Some("1.2026.07131214.177108b3")), description = "Sireum HAMR version"),
     Opt(name = "agree", longKey = "agree", shortKey = None(),
       tpe = Type.Str(sep = None(), default = Some("2.11.2")), description = "AGREE version"),
     Opt(name = "briefcase", longKey = "briefcase", shortKey = None(),
--- fmide.cmd
+++ fmide.cmd
@@ -51,11 +51,11 @@ import Cli._
           |
           |Available Options:
           |    --awas               AWAS version (expects a string; default is
-          |                           "1.2026.05201117.a8293090")
+          |                           "1.2026.07131214.177108b3")
           |    --gumbo              Sireum GUMBO version (expects a string; default is
           |                           "1.2026.06240925.18efbba7")
           |    --hamr               Sireum HAMR version (expects a string; default is
-          |                           "1.2026.05201117.a8293090")
+          |                           "1.2026.07131214.177108b3")
           |    --agree              AGREE version (expects a string; default is "2.11.2")
           |    --briefcase          BriefCASE version (expects a string; default is
           |                           "0.9.2")
@@ -75,9 +75,9 @@ import Cli._
           |-v, --verbose            Verbose output
           |    --verbose+           Increased verbose output""".render
 
-    var awas: Option[String] = Some("1.2026.05201117.a8293090")
+    var awas: Option[String] = Some("1.2026.07131214.177108b3")
     var gumbo: Option[String] = Some("1.2026.06240925.18efbba7")
-    var hamr: Option[String] = Some("1.2026.05201117.a8293090")
+    var hamr: Option[String] = Some("1.2026.07131214.177108b3")
     var agree: Option[String] = Some("2.11.2")
     var briefcase: Option[String] = Some("0.9.2")
     var jkind: Option[String] = Some("4.5.2")
